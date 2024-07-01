6d53cfc45986f15a8f418ea7322b2624d12ee399
shauravbaramu <sauravmagar2000@gmail.com>
2024-07-01 16:06:50 -0400

    Added Styles.css

--- src/main/java/com/example/javafx/HelloApplication.java
+++ src/main/java/com/example/javafx/HelloApplication.java
@@ -11,8 +11,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load(), 800, 530);
-        stage.setTitle("Hello This is my first JavaFx Program!");
+        scene.getStylesheets().add("styles.css");
+        stage.setTitle("myResume Login");
         stage.setScene(scene);
         stage.show();
     }
