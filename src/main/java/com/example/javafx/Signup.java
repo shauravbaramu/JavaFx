536877efdf2bb734bffb8896cc48988a92e9ac62
shauravbaramu <sauravmagar2000@gmail.com>
2024-07-05 19:38:05 -0400

    Signup UI

--- src/main/java/com/example/javafx/Signup.java
+++ src/main/java/com/example/javafx/Signup.java
@@ -1,4 +1,27 @@
 package com.example.javafx;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class Signup {
+import java.io.IOException;
+
+
+public class Signup extends Application{
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Signup.class.getResource("signup-view.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 800, 530);
+
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setTitle("myResume Signup");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
