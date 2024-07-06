17a3a7d44ed66578e5f9e9c1f1ec02d82918bcf5
shauravbaramu <sauravmagar2000@gmail.com>
2024-07-06 19:58:45 -0400

    Issuse Fixed temp

--- src/main/java/com/example/javafx/HelloApplication.java
+++ src/main/java/com/example/javafx/HelloApplication.java
@@ -1,14 +1,15 @@
 package com.example.javafx;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
