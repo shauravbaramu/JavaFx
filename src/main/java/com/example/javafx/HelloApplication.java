4b847ac4d28c0d5d65cacce2e31ce6687900b72c
shauravbaramu <sauravmagar2000@gmail.com>
2024-07-08 20:39:35 -0400

    Close and Minimize button added

--- src/main/java/com/example/javafx/HelloApplication.java
+++ src/main/java/com/example/javafx/HelloApplication.java
@@ -1,10 +1,10 @@
 package com.example.javafx;
 import javafx.application.Application;
-import javafx.fxml.FXML;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 
-import javafx.scene.input.MouseEvent;
+
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import java.io.IOException;
